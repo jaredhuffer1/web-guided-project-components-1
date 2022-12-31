a8d5f25d3634fc99f0f17b7994e1dc2be963bc0a
jaredhuffer1 <jaredhuffer1@gmail.com>
2022-12-31 18:43:09 -0500

    need to finish stretch

--- src/index.js
+++ src/index.js
@@ -31,22 +31,22 @@ const {open, close} = constants;
 //console.log(constants);
 
 // TASK 2- Verify our imports using log statements
-//console.log(panelData);// log the panelData
-//console.log(linkData);// log the linkData
-//console.log(open); // log the open arrow
-//console.log(close); // log the close arrow
+console.log(panelData);// log the panelData
+console.log(linkData);// log the linkData
+console.log(open); // log the open arrow
+console.log(close); // log the close arrow
 
 
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
 //  We will generate the panel with code, and we'll need the parent
 //  so we can append the code-generated panel to the DOM.
 const accordion = document.querySelector(".accordion");
-console.log(accordion);
+//console.log(accordion);
 
 
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
-function makePanel(/* what data does the panel need? */) {
+function makePanel({title, content}) {
 
 
   // TASK 5- Instantiate all the elements needed for a panel
@@ -81,31 +81,59 @@ panelButtons.appendChild(closeButton);
 
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
+panel.classList.add('panel');
+panelBar.classList.add('panel-bar');
+panelButtons.classList.add('panel-buttons');
+openButton.classList.add("panel-btn-open");
+closeButton.classList.add('panel-btn-close', 'hide-btn');
+panelContent.classList.add('panel-content');
 
 
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
-
+panelTitle.textContent = title;
+panelContent.textContent = content;
+openButton.textContent = open;
+closeButton.textContent = close;
 
   // TASK 9- When the 'open' or 'close' buttons are clicked, the content is toggled on/off:
   //  - the open button needs to go away (the 'hide-btn' class name controls this)
   //  - the close button needs to show (the 'hide-btn' class name controls this)
   //  - the contents need to show (the 'toggle-on' class name controls this)
-
+  
+  panelButtons.addEventListener('click', () => {
+    openButton.classList.toggle('hide-btn');
+    closeButton.classList.toggle('hide-btn');
+    panelContent.classList.toggle('toggle-on');
+    })
+  
 
   // don't forget to return the panel!
   return panel;
 }
-const testPanel = makePanel({title: "foo", content: "bar"});
-accordion.appendChild(testPanel);
+
+
 
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
 //  We can do this with a single forEach, or with a map and a forEach.
+const panelElements = panelData.map(panelObj => {
+  return makePanel(panelObj);
+})
+console.log(panelElements);
 
-
+panelElements.forEach((panelElement => {
+accordion.appendChild(panelElement);
+}));
 // [STRETCH] Comment out the links inside the nav and
 // write a linkMaker that takes { href, className, text }
 // and returns an anchor tag with the right href, class and textContent.
 // Loop over the 'linkData' in the data folder, generate anchor tags
 // and append them to the nav.
+/*        <a href="#" class="nav-item">Home</a>
+          <a href="#" class="nav-item">About</a>
+          <a href="#" class="nav-item">Blog</a>
+          <a href="#" class="nav-item">Contact</a>
+*/
+
+
